22911726c9e1518d3cd7826d3da5eb40498f321a
balanithinmsthebest-arch <bala.nithin.m.s.the.best@gmail.com>
2025-12-03 16:19:05 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,1 +1,10 @@
+      const cityLat = 13.0827;
+      const cityLon = 80.2707;
+      const map = L.map("map").setView([cityLat, cityLon], 12);
 
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution: "&copy; OpenStreetMap contributors",
+      }).addTo(map);
+
+      L.marker([cityLat, cityLon]).addTo(map).bindPopup("Chennai").openPopup();
